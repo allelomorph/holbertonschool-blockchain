59b1ccec7f559a3334c20787293c4b27c29ad0d4
Samuel Pomeroy <1690@holbertonschool.com>
2021-07-18 03:03:22 +0000

    0x00 added provided version of _print_hex_buffer.c

--- crypto/provided/_print_hex_buffer.c
+++ crypto/provided/_print_hex_buffer.c
@@ -1,43 +1,17 @@
-/* size_t */
 #include <stddef.h>
-/* uint8_t */
-#include <stdint.h>
-/* malloc free */
-#include <stdlib.h>
-/* fprintf sprintf printf */
 #include <stdio.h>
-
-
-/*
- * Note: File _print_hex_buffer.c mentioned in (443) 0x00. Blockchain - Crypto
- * as a provided file, but could not be found in project outline, so this
- * version was written by the student, inferring output from examples.
-*/
+#include <stdint.h>
 
 /**
- * _print_hex_buffer - prints an array of uint8_t values in 2-digit lowercase
- *   hex format
+ * _print_hex_buffer - Prints a buffer in its hexadecimal form
  *
- * @hex_buf: buffer of values to print
- * @hex_buf_sz: amount of bytes in buffer
+ * @buf: Pointer to the buffer to be printed
+ * @len: Number of bytes from @buf to be printed
  */
-void _print_hex_buffer(uint8_t *hex_buf, size_t hex_buf_sz)
+void _print_hex_buffer(uint8_t const *buf, size_t len)
 {
-	size_t i, j, print_buf_sz;
-	char *print_buf;
+	size_t i;
 
-	print_buf_sz = (hex_buf_sz * 2) + 1;
-	print_buf = malloc(sizeof(char) * print_buf_sz);
-	if (!print_buf)
-	{
-		fprintf(stderr, "_print_hex_buffer: malloc failure\n");
-		return;
-	}
-
-	for (i = 0, j = 0; i < hex_buf_sz && j < print_buf_sz; i++, j += 2)
-		sprintf(print_buf + j, "%02x", hex_buf[i]);
-	print_buf[print_buf_sz - 1] = '\0';
-
-	printf("%s", print_buf);
-	free(print_buf);
+	for (i = 0; buf && i < len; i++)
+		printf("%02x", buf[i]);
 }
