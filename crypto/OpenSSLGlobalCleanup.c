e53262f6f94f199187a32e5288aeb36f054d04a4
Samuel Pomeroy <1690@holbertonschool.com>
2021-07-21 17:58:18 +0000

    0x00 moved global cleanup ((destructor)) to separate file

---
+++ crypto/OpenSSLGlobalCleanup.c
@@ -0,0 +1,47 @@
+/* assumes openssl and libssl-dev versions "1.0.1f-1ubuntu2.27 amd64" */
+/* OPENSSL_VERSION_NUMBER: 0x01000106F */
+/* SSLEAY_VERSION: 'OpenSSL 1.0.1f 6 Jan 2014' */
+
+
+/* ERR_free_strings */
+/* #include <openssl/err.h> */
+/* EVP_cleanup */
+/* #include <openssl/evp.h> */
+/* CRYPTO_cleanup_all_ex_data */
+#include <openssl/crypto.h>
+
+
+/**
+ * OpenSSLGlobalCleanup - runs "brutal" (thread unsafe) application-global
+ *   cleanup functions to free any internal tables allocated by OpenSSL
+ *
+ * Note: It has been observed that the following function subroutine calls:
+ *     - PEM_write* -> BIO_new -> BIO_set -> ...
+ *     - PEM_read* -> PEM_ASN1_read -> BIO_new -> ...
+ *     - ECDSA_sign -> ECDSA_sign_ex -> ECDSA_do_sign_ex -> ...
+ *   all end with -> CRYPTO_malloc -> malloc, allocating data that persists
+ *   until the process ends with a seeming leak of `still reachable: 416
+ *   bytes in 6 blocks`. At `https://www.openssl.org/docs/faq.html#PROG14`,
+ *   "I think I've detected a memory leak...", this is described as "an OpenSSL
+ *   internal table that is allocated when an application starts up". The three
+ *   functions below are listed there as application-global, non-thread safe
+ *   options for freeing these tables.
+ *
+ *   See also `https://stackoverflow.com/a/29927669` regarding best practices
+ *   for OpenSSL 1.0.1 initialization/uninitialization.
+ */
+void OpenSSLGlobalCleanup(void)
+{
+	/* after calls to ERR_load_crypto_strings, SSL_load_error_strings: */
+	/* ERR_free_strings(); */
+
+	/* after calls to OpenSSL_add_all_digests/_algorithms/_ciphers: */
+	/* EVP_cleanup(); */
+
+	/*
+	 * no 1.1.1 man page, but see above links plus:
+	 * `https://www.openssl.org/docs/man1.1.1/man3/CRYPTO_EX_free.html`
+	 */
+	/* after calls to CRYPTO_malloc: */
+	CRYPTO_cleanup_all_ex_data();
+}
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -84,4 +84,8 @@ uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 	      sig_t const *sig);
 
+/* OpenSSLGlobalCleanup.c */
+void OpenSSLGlobalCleanup(void) __attribute__ ((destructor));
+
+
 #endif /* HBLK_CRYPTO_H */
