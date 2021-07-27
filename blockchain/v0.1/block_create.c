2b21b6eb54e162e3e8193b7814d6a6cc74854335
Samuel Pomeroy <1690@holbertonschool.com>
2021-07-27 07:30:45 +0000

    0x01 t1 first attempt

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -1,4 +1,13 @@
+/* block_t */
 #include "blockchain.h"
+/* fprintf */
+#include <stdio.h>
+/* calloc */
+#include <stdlib.h>
+/* time */
+#include <time.h>
+/* memcpy */
+#include <string.h>
 
 
 /**
@@ -14,4 +23,28 @@
 block_t *block_create(block_t const *prev, int8_t const *data,
 		      uint32_t data_len)
 {
+	block_t *new_blk;
+
+	/* prev should never be NULL as "empty" chain has a Genesis Block */
+	if (!prev || !data)
+	{
+		fprintf(stderr, "block_create: NULL parameter(s)\n");
+		return (NULL);
+	}
+
+	new_blk = calloc(1, sizeof(block_t));
+	if (!new_blk)
+	{
+		fprintf(stderr, "block_create: calloc failure\n");
+		return (NULL);
+	}
+
+	new_blk->info.index = prev->info.index + 1;
+	new_blk->info.timestamp = (uint64_t)time(NULL);
+	memcpy(new_blk->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
+	new_blk->data.len = data_len < BLOCKCHAIN_DATA_MAX ?
+		data_len : BLOCKCHAIN_DATA_MAX;
+	memcpy(new_blk->data.buffer, data, new_blk->data.len);
+
+	return (new_blk);
 }
