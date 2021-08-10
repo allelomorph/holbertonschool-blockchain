5ce667ccd16b70d652d76f0e27bd639c1fbfb51e
Samuel Pomeroy <1690@holbertonschool.com>
2021-08-10 07:11:29 +0000

    0x03 t0 first attempt

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -1,6 +1,12 @@
+/* -> blockchain.h -> */
+/* llist.h stdint.h hblk_crypto.h */
 #include "transaction.h"
 /* fprintf */
 #include <stdio.h>
+/* malloc free */
+#include <stdlib.h>
+/* memcpy */
+#include <string.h>
 
 
 /**
@@ -14,9 +20,31 @@
  */
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 {
+	tx_out_t *new_tx_out;
+
 	if (!pub)
 	{
 		fprintf(stderr, "tx_out_create: NULL parameter\n");
 		return (NULL);
 	}
+
+	new_tx_out = malloc(sizeof(tx_out_t));
+	if (!new_tx_out)
+	{
+		fprintf(stderr, "tx_out_create: malloc failure\n");
+		return (NULL);
+	}
+
+	new_tx_out->amount = amount;
+	memcpy(new_tx_out->pub, pub, EC_PUB_LEN);
+
+	if (!sha256((const int8_t *)new_tx_out, sizeof(uint32_t) + EC_PUB_LEN,
+		   new_tx_out->hash))
+	{
+		fprintf(stderr, "tx_out_create: sha256 failure\n");
+		free(new_tx_out);
+		return (NULL);
+	}
+
+	return (new_tx_out);
 }
