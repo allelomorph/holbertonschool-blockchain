f11563d04314da76497361ee3a86a22d6f41c926
Samuel Pomeroy <1690@holbertonschool.com>
2021-08-10 07:11:44 +0000

    0x03 t1 first attempt

--- blockchain/v0.3/transaction/unspent_tx_out_create.c
+++ blockchain/v0.3/transaction/unspent_tx_out_create.c
@@ -1,6 +1,12 @@
+/* -> blockchain.h -> */
+/* llist.h stdint.h hblk_crypto.h */
 #include "transaction.h"
 /* fprintf */
 #include <stdio.h>
+/* malloc */
+#include <stdlib.h>
+/* memcpy */
+#include <string.h>
 
 
 /**
@@ -20,9 +26,26 @@ unspent_tx_out_t *unspent_tx_out_create(
 	uint8_t block_hash[SHA256_DIGEST_LENGTH],
 	uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out)
 {
+	unspent_tx_out_t *new_unspent_tx_out;
+
 	if (!block_hash || !tx_id || !out)
 	{
 		fprintf(stderr, "unspent_tx_out_create: NULL parameter(s)\n");
 		return (NULL);
 	}
+
+	new_unspent_tx_out = malloc(sizeof(unspent_tx_out_t));
+	if (!new_unspent_tx_out)
+	{
+		fprintf(stderr, "unspent_tx_out_create: malloc failure\n");
+		return (NULL);
+	}
+
+	memcpy(&(new_unspent_tx_out->block_hash), block_hash,
+	       SHA256_DIGEST_LENGTH);
+	memcpy(&(new_unspent_tx_out->tx_id), tx_id,
+	       SHA256_DIGEST_LENGTH);
+	memcpy(&(new_unspent_tx_out->out), out, sizeof(tx_out_t));
+
+	return (new_unspent_tx_out);
 }
