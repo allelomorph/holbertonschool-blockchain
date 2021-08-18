f38a3c973c04882833b3645b1313fa33942c7c0d
Samuel Pomeroy <1690@holbertonschool.com>
2021-08-18 04:45:17 +0000

    0x03 t9 first attempt

--- blockchain/v0.3/transaction/transaction_destroy.c
+++ blockchain/v0.3/transaction/transaction_destroy.c
@@ -1,4 +1,8 @@
+/* -> blockchain.h -> */
+/* llist.h stdint.h hblk_crypto.h */
 #include "transaction.h"
+/* free */
+#include <stdlib.h>
 
 
 /**
@@ -8,4 +12,10 @@
  */
 void transaction_destroy(transaction_t *transaction)
 {
+	if (!transaction)
+		return;
+
+	llist_destroy(transaction->inputs, 1, NULL);
+	llist_destroy(transaction->outputs, 1, NULL);
+	free(transaction);
 }
